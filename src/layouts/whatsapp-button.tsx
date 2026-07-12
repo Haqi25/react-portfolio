e71056f7eac8ba8c32c69e6213a553a56329fcef
Haqi25 <baihaqi1401@student.ub.ac.id>
2026-07-12 14:36:11 +0700

    fix: update avatar import in WhatsApp button component

--- src/layouts/whatsapp-button.tsx
+++ src/layouts/whatsapp-button.tsx
@@ -1,14 +1,14 @@
 
 
 import { FloatingWhatsApp } from 'react-floating-whatsapp';
-import json from "../assets/carbon (1).png"
+
 export default function Whatsapp() {
  return (
    <div>
      <FloatingWhatsApp
        phoneNumber="+62895629540686"
        accountName="Hawi"
-       avatar={json}
+       avatar="../assets/carbon (1).png"
        statusMessage="Typically replies within 1 hour"
        chatMessage="Halo dengan Hawi disini ada yang bisa saya bantu 😊"
        placeholder="Type a message.."
