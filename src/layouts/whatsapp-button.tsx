40889e1467c848d3b2c53f5036e525e4c1a298d8
Haqi25 <baihaqi1401@student.ub.ac.id>
2026-07-12 14:50:28 +0700

    update WhatsApp button: change avatar source to external URL

--- src/layouts/whatsapp-button.tsx
+++ src/layouts/whatsapp-button.tsx
@@ -8,7 +8,8 @@ export default function Whatsapp() {
      <FloatingWhatsApp
        phoneNumber="+62895629540686"
        accountName="Hawi"
-       avatar="../assets/carbon (1).png"
+       avatar="https://th.bing.com/th/id/OIP.Pa8EVfDVKkr1vZJsLjSHFwHaHa?w=182&h=182&c=7&r=0&o=7&pid=1.7&rm=3"
+       //https://as2.ftcdn.net/v2/jpg/05/89/93/27/1000_F_589932782_vQAEAZhHnq1QCGu5ikwrYaQD0Mmurm0N.jpg --> this is default profile
        statusMessage="Typically replies within 1 hour"
        chatMessage="Halo dengan Hawi disini ada yang bisa saya bantu 😊"
        placeholder="Type a message.."
