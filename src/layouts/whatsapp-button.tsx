6520dece95d426ea1a8b066bca8e0ecd03a643be
Haqi25 <baihaqi1401@student.ub.ac.id>
2026-07-12 14:30:23 +0700

    add identity json format

--- src/layouts/whatsapp-button.tsx
+++ src/layouts/whatsapp-button.tsx
@@ -1,13 +1,14 @@
 
 
 import { FloatingWhatsApp } from 'react-floating-whatsapp';
+import json from "../assets/carbon (1).png"
 export default function Whatsapp() {
  return (
    <div>
      <FloatingWhatsApp
        phoneNumber="+62895629540686"
        accountName="Hawi"
-       avatar="../assets/hero.png"
+       avatar={json}
        statusMessage="Typically replies within 1 hour"
        chatMessage="Halo dengan Hawi disini ada yang bisa saya bantu 😊"
        placeholder="Type a message.."
