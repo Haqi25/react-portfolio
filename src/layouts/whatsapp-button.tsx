6924f7dd15ba044eec95f7a8b44df3795ae7fd10
Haqi25 <baihaqi1401@student.ub.ac.id>
2026-07-12 14:18:44 +0700

    update WhatsApp button: change avatar URL to use external link

--- src/layouts/whatsapp-button.tsx
+++ src/layouts/whatsapp-button.tsx
@@ -7,7 +7,8 @@ export default function Whatsapp() {
      <FloatingWhatsApp
        phoneNumber="+62895629540686"
        accountName="Hawi"
-       avatar="/src/assets/profile.webp"
+       avatar="https://react-portfolio-beryl-psi-81.vercel.app/src/assets/profile.webp"
+      //     avatar="/src/assets/profile.webp"  ---> you can change into this
        statusMessage="Typically replies within 1 hour"
        chatMessage="Halo dengan Hawi disini ada yang bisa saya bantu 😊"
        placeholder="Type a message.."
